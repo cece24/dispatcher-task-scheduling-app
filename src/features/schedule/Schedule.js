9ae502cd56caaef8ee361d85424944e9f4074ac0
Cecilia Wong <ceciliakhwong@gmail.com>
2020-07-29 14:16:46 -0400

    Adds week selector buttons to set week from 1 to 52 only

--- src/features/schedule/Schedule.js
+++ src/features/schedule/Schedule.js
@@ -51,173 +51,193 @@ const Hour = styled.div`
   height: 40px;
 `;
 
-export function Schedule() {
-  // const DAYS_OF_THE_WEEK = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-  const DAYS_OF_THE_WEEK = [
-    {
-      dayId: 0,
-      label: "MON",
-    },
-    {
-      dayId: 1,
-      label: "TUES",
-    },
-    {
-      dayId: 2,
-      label: "WEDS",
-    },
-    {
-      dayId: 3,
-      label: "THURS",
-    },
-    {
-      dayId: 4,
-      label: "FRI",
-    },
-    {
-      dayId: 5,
-      label: "SAT",
-    },
-    {
-      dayId: 6,
-      label: "SUN",
-    },
-  ];
+// const DAYS_OF_THE_WEEK = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+const DAYS_OF_THE_WEEK = [
+  {
+    dayId: 0,
+    label: "MON",
+  },
+  {
+    dayId: 1,
+    label: "TUES",
+  },
+  {
+    dayId: 2,
+    label: "WEDS",
+  },
+  {
+    dayId: 3,
+    label: "THURS",
+  },
+  {
+    dayId: 4,
+    label: "FRI",
+  },
+  {
+    dayId: 5,
+    label: "SAT",
+  },
+  {
+    dayId: 6,
+    label: "SUN",
+  },
+];
 
-  const HOURS_OF_THE_DAY = [
-    {
-      hourId: 0,
-      label: "12am",
-    },
-    {
-      hourId: 1,
-      label: "1am",
-    },
-    {
-      hourId: 2,
-      label: "2am",
-    },
-    {
-      hourId: 3,
-      label: "3am",
-    },
-    {
-      hourId: 4,
-      label: "4am",
-    },
-    {
-      hourId: 5,
-      label: "5am",
-    },
-    {
-      hourId: 6,
-      label: "6am",
-    },
-    {
-      hourId: 7,
-      label: "7am",
-    },
-    {
-      hourId: 8,
-      label: "8am",
-    },
-    {
-      hourId: 9,
-      label: "9am",
-    },
-    {
-      hourId: 10,
-      label: "10am",
-    },
-    {
-      hourId: 11,
-      label: "11am",
-    },
-    {
-      hourId: 12,
-      label: "12pm",
-    },
-    {
-      hourId: 13,
-      label: "1pm",
-    },
-    {
-      hourId: 14,
-      label: "2pm",
-    },
-    {
-      hourId: 15,
-      label: "3pm",
-    },
-    {
-      hourId: 16,
-      label: "4pm",
-    },
-    {
-      hourId: 17,
-      label: "5pm",
-    },
-    {
-      hourId: 18,
-      label: "6pm",
-    },
-    {
-      hourId: 19,
-      label: "7pm",
-    },
-    {
-      hourId: 20,
-      label: "8pm",
-    },
-    {
-      hourId: 21,
-      label: "9pm",
-    },
-    {
-      hourId: 22,
-      label: "10pm",
-    },
-    {
-      hourId: 23,
-      label: "11pm",
-    },
-  ];
+const HOURS_OF_THE_DAY = [
+  {
+    hourId: 0,
+    label: "12am",
+  },
+  {
+    hourId: 1,
+    label: "1am",
+  },
+  {
+    hourId: 2,
+    label: "2am",
+  },
+  {
+    hourId: 3,
+    label: "3am",
+  },
+  {
+    hourId: 4,
+    label: "4am",
+  },
+  {
+    hourId: 5,
+    label: "5am",
+  },
+  {
+    hourId: 6,
+    label: "6am",
+  },
+  {
+    hourId: 7,
+    label: "7am",
+  },
+  {
+    hourId: 8,
+    label: "8am",
+  },
+  {
+    hourId: 9,
+    label: "9am",
+  },
+  {
+    hourId: 10,
+    label: "10am",
+  },
+  {
+    hourId: 11,
+    label: "11am",
+  },
+  {
+    hourId: 12,
+    label: "12pm",
+  },
+  {
+    hourId: 13,
+    label: "1pm",
+  },
+  {
+    hourId: 14,
+    label: "2pm",
+  },
+  {
+    hourId: 15,
+    label: "3pm",
+  },
+  {
+    hourId: 16,
+    label: "4pm",
+  },
+  {
+    hourId: 17,
+    label: "5pm",
+  },
+  {
+    hourId: 18,
+    label: "6pm",
+  },
+  {
+    hourId: 19,
+    label: "7pm",
+  },
+  {
+    hourId: 20,
+    label: "8pm",
+  },
+  {
+    hourId: 21,
+    label: "9pm",
+  },
+  {
+    hourId: 22,
+    label: "10pm",
+  },
+  {
+    hourId: 23,
+    label: "11pm",
+  },
+];
 
-  const taskTimeId = "1-12";
+const taskTimeId = "1-12";
 
-  return (
-    <ScheduleContainer>
-      <Header>
-        <Button>Driver Selector</Button>
-        <div>WEEK #</div>
-        <Button>Download Schedule</Button>
-      </Header>
-      <Body>
-        <DayContainer>
-          <Hours style={{ marginTop: "40px" }}>
-            {HOURS_OF_THE_DAY.map((h) => (
-              <Hour key={h}>{h.label}</Hour>
-            ))}
-          </Hours>
-        </DayContainer>
-        {DAYS_OF_THE_WEEK.map((d) => (
-          <DayContainer key={d}>
-            <Day>
-              <strong>{d.label}</strong>
-            </Day>
-            <Hours>
+export class Schedule extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      week: 1,
+    };
+  }
+
+  setWeek = (direction) => {
+    if (direction === "previous" && this.state.week !== 1) {
+      this.setState({ week: this.state.week - 1 });
+    } else if (direction === "next" && this.state.week !== 52) {
+      this.setState({ week: this.state.week + 1 });
+    }
+  };
+
+  render() {
+    return (
+      <ScheduleContainer>
+        <Header>
+          <Button>Driver Selector</Button>
+          <Button onClick={() => this.setWeek("previous")}>{"<"}</Button>
+          <div>WEEK {this.state.week}</div>
+          <Button onClick={() => this.setWeek("next")}>{">"}</Button>
+          <Button>Download Schedule</Button>
+        </Header>
+        <Body>
+          <DayContainer>
+            <Hours style={{ marginTop: "40px" }}>
               {HOURS_OF_THE_DAY.map((h) => (
-                <Hour key={h} id={`${d.dayId}-${h.hourId}`}>
-                  {h.label}
-                  {`${d.dayId}-${h.hourId}` === taskTimeId && (
-                    <span>has task</span>
-                  )}
-                </Hour>
+                <Hour key={h}>{h.label}</Hour>
               ))}
             </Hours>
           </DayContainer>
-        ))}
-      </Body>
-    </ScheduleContainer>
-  );
+          {DAYS_OF_THE_WEEK.map((d) => (
+            <DayContainer key={d}>
+              <Day>
+                <strong>{d.label}</strong>
+              </Day>
+              <Hours>
+                {HOURS_OF_THE_DAY.map((h) => (
+                  <Hour key={h} id={`${d.dayId}-${h.hourId}`}>
+                    {h.label}
+                    {`${d.dayId}-${h.hourId}` === taskTimeId && (
+                      <span>has task</span>
+                    )}
+                  </Hour>
+                ))}
+              </Hours>
+            </DayContainer>
+          ))}
+        </Body>
+      </ScheduleContainer>
+    );
+  }
 }
