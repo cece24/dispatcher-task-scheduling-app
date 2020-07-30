b14fd4902d8e8ee15f51681c7a4fd9311d79aa83
Cecilia Wong <ceciliakhwong@gmail.com>
2020-07-29 21:45:26 -0400

    Refactor redux store, small modifications to index styling

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+
+import Schedule from "./features/schedule/Schedule";
+
+import tasks from "./features/task/tasks";
+
+const store = createStore(combineReducers({ tasks }));
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Schedule />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
