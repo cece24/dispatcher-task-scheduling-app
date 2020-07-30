7db98c4c2411825a934b01fdd7cec5083d2c100d
Cecilia Wong <ceciliakhwong@gmail.com>
2020-07-29 21:44:14 -0400

    Create constants file to days and hours constants

---
+++ src/app/constants.js
@@ -0,0 +1,129 @@
+export const DAYS_OF_THE_WEEK = [
+  {
+    dayId: 0,
+    label: "MON",
+  },
+  {
+    dayId: 1,
+    label: "TUES",
+  },
+  {
+    dayId: 2,
+    label: "WEDS",
+  },
+  {
+    dayId: 3,
+    label: "THURS",
+  },
+  {
+    dayId: 4,
+    label: "FRI",
+  },
+  {
+    dayId: 5,
+    label: "SAT",
+  },
+  {
+    dayId: 6,
+    label: "SUN",
+  },
+];
+
+export const HOURS_OF_THE_DAY = [
+  {
+    hourId: 0,
+    label: "12am",
+  },
+  {
+    hourId: 1,
+    label: "1am",
+  },
+  {
+    hourId: 2,
+    label: "2am",
+  },
+  {
+    hourId: 3,
+    label: "3am",
+  },
+  {
+    hourId: 4,
+    label: "4am",
+  },
+  {
+    hourId: 5,
+    label: "5am",
+  },
+  {
+    hourId: 6,
+    label: "6am",
+  },
+  {
+    hourId: 7,
+    label: "7am",
+  },
+  {
+    hourId: 8,
+    label: "8am",
+  },
+  {
+    hourId: 9,
+    label: "9am",
+  },
+  {
+    hourId: 10,
+    label: "10am",
+  },
+  {
+    hourId: 11,
+    label: "11am",
+  },
+  {
+    hourId: 12,
+    label: "12pm",
+  },
+  {
+    hourId: 13,
+    label: "1pm",
+  },
+  {
+    hourId: 14,
+    label: "2pm",
+  },
+  {
+    hourId: 15,
+    label: "3pm",
+  },
+  {
+    hourId: 16,
+    label: "4pm",
+  },
+  {
+    hourId: 17,
+    label: "5pm",
+  },
+  {
+    hourId: 18,
+    label: "6pm",
+  },
+  {
+    hourId: 19,
+    label: "7pm",
+  },
+  {
+    hourId: 20,
+    label: "8pm",
+  },
+  {
+    hourId: 21,
+    label: "9pm",
+  },
+  {
+    hourId: 22,
+    label: "10pm",
+  },
+  {
+    hourId: 23,
+    label: "11pm",
+  },
+];
--- src/features/schedule/Schedule.js
+++ src/features/schedule/Schedule.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import { DAYS_OF_THE_WEEK, HOURS_OF_THE_DAY } from "../../app/constants";
+
 const ScheduleContainer = styled.div`
   display: inline-block;
   width: 70vw;
@@ -51,139 +53,6 @@ const Hour = styled.div`
   height: 40px;
 `;
 
-// const DAYS_OF_THE_WEEK = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-const DAYS_OF_THE_WEEK = [
-  {
-    dayId: 0,
-    label: "MON",
-  },
-  {
-    dayId: 1,
-    label: "TUES",
-  },
-  {
-    dayId: 2,
-    label: "WEDS",
-  },
-  {
-    dayId: 3,
-    label: "THURS",
-  },
-  {
-    dayId: 4,
-    label: "FRI",
-  },
-  {
-    dayId: 5,
-    label: "SAT",
-  },
-  {
-    dayId: 6,
-    label: "SUN",
-  },
-];
-
-const HOURS_OF_THE_DAY = [
-  {
-    hourId: 0,
-    label: "12am",
-  },
-  {
-    hourId: 1,
-    label: "1am",
-  },
-  {
-    hourId: 2,
-    label: "2am",
-  },
-  {
-    hourId: 3,
-    label: "3am",
-  },
-  {
-    hourId: 4,
-    label: "4am",
-  },
-  {
-    hourId: 5,
-    label: "5am",
-  },
-  {
-    hourId: 6,
-    label: "6am",
-  },
-  {
-    hourId: 7,
-    label: "7am",
-  },
-  {
-    hourId: 8,
-    label: "8am",
-  },
-  {
-    hourId: 9,
-    label: "9am",
-  },
-  {
-    hourId: 10,
-    label: "10am",
-  },
-  {
-    hourId: 11,
-    label: "11am",
-  },
-  {
-    hourId: 12,
-    label: "12pm",
-  },
-  {
-    hourId: 13,
-    label: "1pm",
-  },
-  {
-    hourId: 14,
-    label: "2pm",
-  },
-  {
-    hourId: 15,
-    label: "3pm",
-  },
-  {
-    hourId: 16,
-    label: "4pm",
-  },
-  {
-    hourId: 17,
-    label: "5pm",
-  },
-  {
-    hourId: 18,
-    label: "6pm",
-  },
-  {
-    hourId: 19,
-    label: "7pm",
-  },
-  {
-    hourId: 20,
-    label: "8pm",
-  },
-  {
-    hourId: 21,
-    label: "9pm",
-  },
-  {
-    hourId: 22,
-    label: "10pm",
-  },
-  {
-    hourId: 23,
-    label: "11pm",
-  },
-];
-
-const taskTimeId = "1-12";
-
 export class Schedule extends React.Component {
   constructor(props) {
     super(props);
@@ -219,19 +88,29 @@ export class Schedule extends React.Component {
               ))}
             </Hours>
           </DayContainer>
-          {DAYS_OF_THE_WEEK.map((d) => (
-            <DayContainer key={d}>
+          {DAYS_OF_THE_WEEK.map((day) => (
+            <DayContainer key={day.dayId}>
               <Day>
-                <strong>{d.label}</strong>
+                <strong>{day.label}</strong>
               </Day>
               <Hours>
-                {HOURS_OF_THE_DAY.map((h) => (
-                  <Hour key={h} id={`${d.dayId}-${h.hourId}`}>
-                    {h.label}
-                    {`${d.dayId}-${h.hourId}` === taskTimeId && (
-                      <span>has task</span>
-                    )}
-                  </Hour>
+                {HOURS_OF_THE_DAY.map((hour, index) => (
+                  <div>
+                    <Hour key={hour.hourId}>
+                      {hour.label}
+                      <Task
+                        active={() => this.hasTask(day.dayId, hour.hourId)}
+                      ></Task>
+                    </Hour>
+                    {/* //   <Hour
+                    //     key={`hour-${index}`}
+                    //     weekId={this.state.week}
+                    //     dayId={day.dayId}
+                    //     hourId={hour.hourId}
+                    //     hourLabel={hour.label}
+                    //     tasks={this.props.tasks}
+                    //   ></Hour> */}
+                  </div>
                 ))}
               </Hours>
             </DayContainer>
